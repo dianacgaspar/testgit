8885625eae0552e970eb69aa5c2058ffdade67fb
dianacgaspar <114398062+dianacgaspar@users.noreply.github.com>
2022-12-08 11:26:10 +0000

    Update testchild.py

--- testchild.py
+++ testchild.py
@@ -1,2 +1,6 @@
-## Adding a new file in child branch
+## This is some other comment
+## Let's see if there are conflicts now
+print ("HEEEEEELLLLLLLLLLLLLLOOOOOOOOOOOOOO !")
+
+## This was the old comment: Adding a new file in child branch
 print ("Inside testbranch - with changes!!!!!")
